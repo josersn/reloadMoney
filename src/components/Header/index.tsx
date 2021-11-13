11bc52cb6ca3c15dde9606472f44d0164de4822d
josersn <nettorammos@hotmail.com>
2021-11-13 11:43:39 -0300

    feat: login header action

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import Logo from "../../assets/logo.png"
 
 import { Button, Container, Content, Menu } from './styles';
 
 function Header() {
+    const [login, setLogin] = useState(true);
+
     return (
         <Container>
             <Content>
                 <img src={Logo} alt="Reload Money" />
                 <div>
-                    <Menu>
-                        <a href="/">Como Funciona</a>
-                        <a href="/">Pontos de coleta</a>
-                        <a href="/">Trocar pontos</a>
-                    </Menu>
+                    {!login && (
+                        <Menu>
+                            <a href="/">Como Funciona</a>
+                            <a href="/">Pontos de coleta</a>
+                            <a href="/">Trocar pontos</a>
+                        </Menu>
+                    )}
                     <Button>
-                        ENTRAR
+                        {login ? "SAIR" : "ENTRAR"}
                     </Button>
                 </div>
             </Content>
