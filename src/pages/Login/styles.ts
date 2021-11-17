8648fc6829cb9e171dcbdc25053ea99ac75998a8
HeitorCau <heitormcau@gmail.com>
2021-11-16 22:04:40 -0300

    Adicionamdo estilo do container de login

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    width:100vw;
-    height: 100vh;
+export const Container = styled.div `
+    width:80vw;
+    height: 80vh;
+    margin: 4.5% auto;
+    background-color: #000;
+    border-radius: 12px;
 `;
