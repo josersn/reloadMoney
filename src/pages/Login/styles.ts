3427885fccfeb6b45bb8f25e316bdf9cefe1a9d1
HeitorCau <heitormcau@gmail.com>
2021-11-18 15:59:31 -0300

    Adicionando estilização ao login

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,9 +1,41 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
+interface MenuOptionProps {
+    selected?: boolean
+}
+
+export const Container = styled.div
     width:80vw;
     height: 80vh;
     margin: 4.5% auto;
     background-color: #000;
     border-radius: 12px;
-`;
+;
+
+export const Content = styled.div
+  width: 50%;
+  height: 100%;
+  border-radius: 12px;
+
+  header {
+      width: 80%;
+      margin: 0 auto;
+      display: flex;
+  }
+
+;
+
+export const MenuOption = styled.span<MenuOptionProps>
+    width: 50%;
+    text-align: center;
+    margin-top: 30px;
+    color: #ffffff;
+    padding: 0 0 8px 0;
+    font-weight: bold;
+    border-bottom: 3px solid ${({ selected }) => selected ? '#00B969' : '#000'};
+;
+
+export const Image = styled.div
+  width: 50%;
+  height: 100%;
+;
