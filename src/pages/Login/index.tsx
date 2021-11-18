6775e4627ce89a76943785acae965e43c2b2b75a
HeitorCau <heitormcau@gmail.com>
2021-11-18 15:56:45 -0300

    Alteração da função login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,26 @@
-import { Container } from "./styles";
+Código: import { useState } from "react";
+import { 
+    Container,
+    Content,
+    Image,
+    MenuOption
+} from "./styles";
 
-function Login() { 
-    return(
+function Login() {
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    return (
         <Container>
-
+            <Content>
+                <header>
+                    <MenuOption selected={isLogin} >Login</MenuOption>
+                    <MenuOption selected={!isLogin}>Cadastro</MenuOption>
+                </header>
+            </Content>
+            <Image></Image>
         </Container>
     )
- }
+}
 
 export { Login }
